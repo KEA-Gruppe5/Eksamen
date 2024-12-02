323b6311ffffb9311ee1f8495af1ac6a49d241ac
ViAarisKea <viaa0001@stud.kea.dk>
2024-12-03 00:46:27 +0100

    add transactional to test and correct task test

--- src/test/java/kea/eksamen/repository/TaskRepositoryTest.java
+++ src/test/java/kea/eksamen/repository/TaskRepositoryTest.java
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 class TaskRepositoryTest {
 
     @Autowired
@@ -91,7 +92,7 @@ class TaskRepositoryTest {
         Task task = taskRepository.findTaskById(1);
 
         assertNotNull(task);
-        assertEquals("Task 1 for Alpha", task.getTitle());
+        assertEquals("Task 1 for Beta", task.getTitle());
         assertEquals("Task description for Alpha 1", task.getDescription());
         assertEquals(TaskPriority.HIGH, task.getPriority());
         assertEquals(1, task.getUserId());
@@ -103,7 +104,7 @@ class TaskRepositoryTest {
 
         assertFalse(tasks.isEmpty());
         Task task = tasks.get(0);
-        assertEquals("Task 1 for Alpha", task.getTitle());
+        assertEquals("Task 1 for Beta", task.getTitle());
         assertEquals("Task description for Alpha 1", task.getDescription());
         assertEquals(TaskPriority.HIGH, task.getPriority());
     }
